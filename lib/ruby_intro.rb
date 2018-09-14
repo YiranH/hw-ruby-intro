1b7e1a98fb3f48e4d8bda58503a8c127ae47525e
YiranH <42849336+YiranH@users.noreply.github.com>
2018-09-13 23:47:24 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,70 @@
 # When done, submit this entire file to the autograder.
 
+# When done, submit this entire file to the autograder.
+
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length==0
+    return 0
+  end
+  s = arr.reduce(:+)
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length==0
+    return 0
+  end
+  return arr.max(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  len=arr.length
+  if(len==0 ||len==1)
+    return false
+  end
+  for i in (0...len)
+    for j in (i+1...len)
+      if arr[i]+arr[j]==n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == "0"
+    return true
+  end
+  return /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    sprintf("$%#.2f", price)
+  end
 end
